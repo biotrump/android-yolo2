000a56f01302abb5c602da776990b22d031dcf50
thomas tsai <thomas@life100.cc>
2018-06-03 14:29:34 +0800

    first init from https://github.com/chentyjpm/DarknetDemoInAndroid.git https://zhuanlan.zhihu.com/p/33822125
    Android yolo v2

---
+++ darknet/examples/test.c
@@ -0,0 +1,10 @@
+//#include "darknet.h"
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+printf("\n\n%s:argc=%d, argv[0]=%s\n",__func__, argc, argv[0]);
+}
